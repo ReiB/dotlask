c3d682137b98e51d86382402e10a08ba26cc7afd
Reinaldo Benitez <reinaldo.benitezm@gmail.com>
2017-09-29 23:38:57 -0400

    minor UI update

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # your processing here
+    return render_template('error.html')
+
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
